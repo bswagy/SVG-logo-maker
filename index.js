33b63483320c06fd39d643654673d3c706d15a32
brian <brian.aguilar235@gmail.com>
2024-03-09 22:25:59 -0800

    user prompts

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 //function to prompt the user for input
 const promptUser = () => {
-return inquirer.prompt([
+ return inquirer.prompt([
+ {
+type: 'input',
+name:'text',
+message: 'Enter the text for your logo (up to three characters):'
+ },
+ { 
+ type: 'input',
+ name:'textColor',
+message: 'Enter the text color (color keyword or hexadecimal number):'
+},
 {
 
+ type: 'list',
+name:'shape',
+message: 'Choose a shape for your logo:',
+choices: ['circle', 'triangle', 'square']
+ },
+{
 
-
-
-
-
-    
-})
+type: 'input',
+name:'shapeColor',
+message: 'Enter the shape color (color keyword or hexadecimal number):'
 }
+ ]);
+};
+
+
