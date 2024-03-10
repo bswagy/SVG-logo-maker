6b3a5364cea579ddf4c16d8ec16d77329487c8bc
brian <brian.aguilar235@gmail.com>
2024-03-09 20:11:08 -0800

    prompts

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//function to prompt the user for input
+const promptUser = () => {
+return inquirer.prompt([
+{
+
+
+
+
+
+
+    
+}
+}
